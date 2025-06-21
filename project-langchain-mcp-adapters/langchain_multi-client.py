c5a04c8b32214c354396ffddd0363a37c4b6689e
avdhut.ssh <avdhut.ssh@gmail.com>
2025-06-21 11:13:49 +0530

    Integrate MultiServerMCPClient and enhance agent invocation with weather query

--- project-langchain-mcp-adapters/langchain_multi-client.py
+++ project-langchain-mcp-adapters/langchain_multi-client.py
@@ -1,10 +1,10 @@
 import asyncio
-import os
+
 from dotenv import load_dotenv
-from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.prebuilt import create_react_agent
-
+from langchain_core.messages import HumanMessage
 
 load_dotenv()
 
@@ -17,6 +17,7 @@ async def main():
             "math": {
                 "command": "python",
                 "args": ["servers/math_server.py"],
+                "transport": "stdio"
             },
             "weather": {
                 "url": "http://localhost:8000/sse",
@@ -24,6 +25,17 @@ async def main():
             },
         }
     )
+    
+    tools = await client.get_tools()
+    agent = create_react_agent(llm, tools)
+    
+    # result = await agent.ainvoke({
+    #     "messages": [HumanMessage(content="What is 54 + 2 * 3?")]
+    # })
+    result = await agent.ainvoke(
+        {"messages": "What is the weather in San Francisco?"}
+    )
+    print(result["messages"][-1].content)
 
 
 if __name__ == "__main__":
